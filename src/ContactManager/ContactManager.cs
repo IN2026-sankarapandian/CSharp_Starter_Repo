9eb9b9c609cc11babde8773fd86ddfe4323571cf
IN2026-sankarapandian <sankarapandian.g@solitontech.com>
2025-07-16 11:45:10 +0530

    Feature Contact Manager console application (Assignment#1)
    * feat: implemented contact manager
    
    * refactor: minimal changes
    
    * refactor: minimal changes
    
    * refactor: minimal changes
    
    * refactor: Changed comments and names
    
    * fix: Added project to solution correctly
    
    * Fix: Removed the projects directories that dont exists
    
    * Fix : Renamed the project name according to vsix template requirement
    
    * Included vsix template
    
    * Fix: Included vsix template to Contact Manager project
    
    * Fix: Added a ContactManager to common sln file
    
    * refactor : combined prompt functions into a single function
    
    * fix: removed extra projects
    
    * Fix: removed the changes in editorconfig
    
    * Fix: removed the changes in editorconfig
    
    * Docs: removed old functions
    
    * Fix : removed unnecessary nullable vars in contact and consoleUI
    
    * docs: updated developer docs
    
    * refactor: removed usnused variables

---
+++ src/ContactManager/ContactManager.cs
@@ -0,0 +1,52 @@
+﻿namespace ContactManager;
+
+/// <summary>
+/// This is the main class of the entire project.
+/// </summary>
+internal class ContactManager
+{
+    /// <summary>
+    /// The Main method is the entry point of a C# application. When the application is started, the Main method is the first method that is invoked.
+    /// </summary>
+    /// <param name="args">Inputs from console while executing file</param>
+    /// <returns>A <see cref="Task"/> Representing the asynchronous operation.</returns>
+    public static async Task Main()
+    {
+        ContactRepository userContactList = new ContactRepository();
+        ConsoleUI.PrintAppHeader("Menu");
+        do
+        {
+            Console.WriteLine("1. Add  \n2. Show  \n3. Edit  \n4. Delete  \n5. Sort  \n6. Search  \n7. Exit\n");
+            string? choice = ConsoleUI.GetInputWithPrompt("What do you want to do : ");
+            switch (choice)
+            {
+                case "1":
+                    FeatureHandler.ManageAddContact(userContactList);
+                    break;
+                case "2":
+                    FeatureHandler.ManageShowContact(userContactList);
+                    break;
+                case "3":
+                    FeatureHandler.ManageEditContact(userContactList);
+                    break;
+                case "4":
+                    FeatureHandler.ManageDeleteContact(userContactList);
+                    break;
+                case "5":
+                    FeatureHandler.ManageSortContact(userContactList);
+                    break;
+                case "6":
+                    FeatureHandler.ManageSearchContact(userContactList);
+                    break;
+                case "7":
+                    ConsoleUI.PrintAppHeader("Closing the application.....");
+                    await Task.Delay(1000);
+                    return;
+                default:
+                    ConsoleUI.PromptInfoWithColor("Enter a valid options !\n", ConsoleColor.Yellow);
+                    break;
+            }
+        }
+        while (true);
+    }
+}
---
+++ src/ContactManager/Repository/ContactRepository.cs
@@ -0,0 +1,144 @@
+﻿namespace ContactManager;
+
+/// <summary>
+/// Create and manages the main list of contacts data and handles core operations
+/// </summary>
+internal class ContactRepository
+{
+    private List<Contact> _contactList = new List<Contact>();
+
+    /// <summary>
+    /// Holds the template of the contact.
+    /// </summary>
+    /// <returns>Template of contact as a dictionary.</returns>
+    public static Dictionary<string, string> GetContactTemplate()
+    {
+        return Contact.GetContactTemplate();
+    }
+
+    /// <summary>
+    /// Adds a new contact object to the list and holds the new contact.
+    /// </summary>
+    /// <param name="contactDetails">Dictionary of contact data.</param>
+    /// <returns>Returns the contact that has been added.</returns>
+    public Contact AddContact(Dictionary<string, string> contactDetails)
+    {
+        Contact newContact = new Contact(contactDetails);
+        this._contactList.Add(newContact);
+        return newContact;
+    }
+
+    /// <summary>
+    /// Holds the entire contact list.
+    /// </summary>
+    /// <returns>Contacts as list.</returns>
+    public List<Contact> GetContacts() => this._contactList;
+
+    /// <summary>
+    /// Holds the contact object of specified index.
+    /// </summary>
+    /// <param name="index">The index of the required contact in list.</param>
+    /// <returns>Contact object in specified index.</returns>
+    public Contact GetContact(int index)
+    {
+        return this._contactList[index];
+    }
+
+    /// <summary>.
+    /// Updates contact based on index, field and new value to change.
+    /// </summary>
+    /// <param name="index">index of contact to be edited</param>
+    /// <param name="field">field of contact to edit</param>
+    /// <param name="newValue">changes</param>
+    public void EditContact(int index, string field, string newValue)
+    {
+        if (index >= this._contactList.Count || index < 0)
+        {
+            return;
+        }
+
+        this._contactList[index][field] = newValue;
+    }
+
+    /// <summary>
+    /// Removes selected contact from list at specified index.
+    /// </summary>
+    /// <param name="index">Index of contact to delete.</param>
+    public void DeleteContacts(int index)
+    {
+        if (index >= this._contactList.Count || index < 0)
+        {
+            return;
+        }
+
+        this._contactList.RemoveAt(index);
+    }
+
+    /// <summary>
+    /// Searches contacts by field with keyword, Holds the matched results as list.
+    /// </summary>
+    /// <param name="field">Field to search for.</param>
+    /// <param name="keyword">Keyword to find matches.</param>
+    /// <returns>Filtered list</returns>
+    public List<Contact> Search(string field, string keyword)
+    {
+        List<Contact> filteredContacts = this._contactList.FindAll(contact =>
+        {
+            if (contact is null || contact[field] is null || field is null)
+            {
+                return false;
+            }
+
+            return contact[field].ToUpper().Contains(keyword.ToUpper());
+        });
+        return filteredContacts;
+    }
+
+    /// <summary>
+    /// Sorts the contact list by field(name, time).
+    /// </summary>
+    /// <param name="field">Field to sort.</param>
+    public void SortContacts(string field)
+    {
+        this._contactList.Sort((contactA, contactB) =>
+        {
+            if (contactA is null || contactB is null)
+            {
+                return 0;
+            }
+
+            return contactA[field].CompareTo(contactB[field]);
+        });
+    }
+
+    /// <summary>
+    /// Checks for duplicate on any specified field and return true if duplicate exist else return false.
+    /// </summary>
+    /// <param name="field">Field to check for in contact list.</param>
+    /// <param name="value">Value to check with existing values.</param>
+    /// <returns>Return true if there is duplicate and false if there is no duplicates.</returns>
+    public bool HaveDuplicate(string field, string value)
+    {
+        if (this._contactList.Exists(contact => contact[field] == value))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// To handle out of range exceptions, check whether the contact exist in a specified index.
+    /// </summary>
+    /// <param name="index">The index value to check with.</param>
+    /// <returns>return true if it is in range and false if index it out of range.</returns>
+    public bool IsValidIndex(int index)
+    {
+        if (index >= 0 && index < this._contactList.Count)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
