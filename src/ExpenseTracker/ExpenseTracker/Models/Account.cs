eb0754ef995925e5b89c1f072e4cb6be3f4e828f
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-07-18 16:06:58 +0530

    local comit

---
+++ src/ExpenseTracker/ExpenseTracker/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace ExpenseTracker.Models;
+
+public class Account
+{
+    public string Name { get; set; }
+    public int CurrentBalance { get; set; }
+
+    public Account(string name, int currentBalance)
+    {
+        Name = name;
+        this.CurrentBalance = currentBalance;
+    }
+
+}
---
+++ src/ExpenseTracker/ExpenseTracker/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿namespace ExpenseTracker.Models;
+
+/// <summary>
+/// This is the interface for transaction based classes
+/// </summary>
+public interface ITransaction
+{
+    /// <summary>
+    /// Gets or sets amount of transaction
+    /// </summary>
+    /// <value>
+    /// Amount of transaction
+    /// </value>
+    int Amount { get; set; }
+
+    /// <summary>
+    /// Account used to transact
+    /// </summary>
+    /// <value>
+    /// Account used to transact</placeholder>
+    /// </value>
+    Account UsedAccount { get; set; }
+
+    /// <summary>
+    /// Apply transaction
+    /// </summary>
+    /// <param name="amount"></param>
+    public void Transact(int amount);
+}
