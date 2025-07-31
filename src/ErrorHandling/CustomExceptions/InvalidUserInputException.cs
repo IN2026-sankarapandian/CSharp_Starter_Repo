42e6f58efc23c4c865c476ebc590ee033f044bbf
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-07-31 09:53:32 +0530

    feat : Added 5 tasks

---
+++ src/ErrorHandling/CustomExceptions/InvalidUserInputException.cs
@@ -0,0 +1,33 @@
+﻿namespace ErrorHandling.CustomExceptions;
+
+/// <summary>
+/// The exception that is shown when user gives invalid input.
+/// </summary>
+public class InvalidUserInputException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidUserInputException"/> class.
+    /// </summary>
+    public InvalidUserInputException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidUserInputException"/> class.
+    /// </summary>
+    /// <param name="message">Message for the exception.</param>
+    public InvalidUserInputException(string? message)
+        : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidUserInputException"/> class.
+    /// </summary>
+    /// <param name="message">Message for the exception.</param>
+    /// <param name="innerException">Inner exception.</param>
+    public InvalidUserInputException(string? message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
---
+++ src/ErrorHandling/Tasks/Task4.cs
@@ -0,0 +1,21 @@
+﻿using ErrorHandling.UI;
+
+namespace ErrorHandling.Tasks;
+
+/// <summary>
+/// Same as <see cref="Task3"/> but might throw an <see cref="IndexOutOfRangeException"/> without handling.
+/// </summary>
+public class Task4
+{
+    /// <summary>
+    /// Entry point to run the task4.
+    /// </summary>
+    public void Run()
+    {
+        Task3 task3 = new ();
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        ConsoleUI.Display(numbers);
+        int selectedIndex = task3.GetUserInput("Select a index : ");
+        Console.WriteLine($"Selected number at index [{selectedIndex}] is {numbers[selectedIndex]}.");
+    }
+}
