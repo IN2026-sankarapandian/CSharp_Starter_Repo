56fcf6ac32dfcc3a391e05dd79589c15c4b31e47
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-08-06 15:30:09 +0530

    fix: removed null forgivers in models

--- src/LINQ/Models/Product.cs
+++ src/LINQ/Models/Product.cs
@@ -5,72 +5,27 @@
 /// </summary>
 public class Product
 {
-    private string _id = null!;
-    private string _name = null!;
-    private decimal _price;
-    private string _category = null!;
-
     /// <summary>
     /// Gets or sets the Id of the product.
     /// </summary>
     /// <value>Holds the Id of the product.</value>
-    public string ID
-    {
-        get => this._id;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Name cant be empty");
-            }
-
-            this._id = value;
-        }
-    }
+    public string? ID { get;  set; }
 
     /// <summary>
     /// Gets or sets the name of the product.
     /// </summary>
     /// <value>Holds the Name of the product.</value>
-    public string Name
-    {
-        get => this._name;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Name cant be empty");
-            }
-
-            this._name = value;
-        }
-    }
+    public string? Name { get; set; }
 
     /// <summary>
     /// Gets or sets the price of the product.
     /// </summary>
     /// <value>Holds the price of the product.</value>
-    public decimal Price
-    {
-        get => this._price;
-        set { this._price = value; }
-    }
+    public decimal Price { get; set; }
 
     /// <summary>
     /// Gets or sets the category of the product.
     /// </summary>
     /// <value>Holds the category of the product.</value>
-    public string Category
-    {
-        get => this._category;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Category cant be empty");
-            }
-
-            this._category = value;
-        }
-    }
+    public string? Category { get; set; }
 }
--- src/LINQ/Models/Supplier.cs
+++ src/LINQ/Models/Supplier.cs
@@ -5,61 +5,21 @@
 /// </summary>
 public class Supplier
 {
-    private string _supplierId = null!;
-    private string _supplierName = null!;
-    private string _productId = null!;
-
     /// <summary>
     /// Gets or sets the Id of the supplier.
     /// </summary>
     /// <value>Holds the Id of the supplier.</value>
-    public string SupplierId
-    {
-        get => this._supplierId;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Supplier id can`t be empty");
-            }
-
-            this._supplierId = value;
-        }
-    }
+    public string? SupplierId { get; set; }
 
     /// <summary>
     /// Gets or sets the name of the supplier name.
     /// </summary>
     /// <value>Holds the Name of the supplier name.</value>
-    public string SupplierName
-    {
-        get => this._supplierName;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Supplier name can`t be empty");
-            }
-
-            this._supplierName = value;
-        }
-    }
+    public string? SupplierName { get; set; }
 
     /// <summary>
     /// Gets or sets the Id of the product.
     /// </summary>
     /// <value>Holds the Id of the product.</value>
-    public string ProductId
-    {
-        get => this._productId;
-        set
-        {
-            if (value is null)
-            {
-                throw new ArgumentNullException("Product Id can`t be empty");
-            }
-
-            this._productId = value;
-        }
-    }
+    public string? ProductId { get; set; }
 }
