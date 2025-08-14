dd87d1a9dc3e7f8101a3fc1f51ed29ddd984db89
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-08-14 11:06:06 +0530

    feat : added arithmetic calculator

---
+++ src/Dotnet/ArithmeticCalculator/Constants/RegexPatterns.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet.ArithmeticCalculator.Constants;
+
+/// <summary>
+/// Have regex validation patterns.
+/// </summary>
+public static class RegexPatterns
+{
+    /// <summary>
+    /// Its and regex pattern to validate a arithmetic expression.
+    /// </summary>
+    public const string ArithmeticExpressionRegex = @"^(-?\d+)\s*([-+*/])\s*(-?\d+)$";
+}
---
+++ src/Dotnet/ArithmeticCalculator/UserInterface/IUserInterface.cs
@@ -0,0 +1,22 @@
+﻿using Dotnet.ArithmeticCalculator.Constants;
+
+namespace Dotnet.ArithmeticCalculator.UserInterface;
+
+/// <summary>
+/// Defines a contract for user interface.
+/// </summary>
+public interface IUserInterface
+{
+    /// <summary>
+    /// Gets user input and returns it.
+    /// </summary>
+    /// <returns>User's input</returns>
+    public string? GetInput();
+
+    /// <summary>
+    /// Shows the message to user as a specified type.
+    /// </summary>
+    /// <param name="message">Message shown to user.</param>
+    /// <param name="type">Type of the message to show.</param>
+    public void ShowMessage(string message, MessageType type);
+}
