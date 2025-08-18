efd1f2ede39142ae7d231238c34bc049663fc354
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-08-18 16:07:29 +0530

    feat: added assignment 12 (Memory optimization)

---
+++ src/MemoryOptimization/MemoryOptimization.cs
@@ -0,0 +1,19 @@
+﻿namespace MemoryOptimization;
+
+/// <summary>
+/// Demonstrates how to diagnose and resolve memory issues.
+/// </summary>
+public class MemoryOptimization
+{
+    /// <summary>
+    /// This is an entry point of <see cref="MemoryOptimization"/> and runs first when the project is executed.
+    /// </summary>
+    public static void Main()
+    {
+        MemoryEaterOptimized.MemoryEater memoryEater = new ();
+        // MemoryEater.MemoryEater memoryEater = new ();
+        memoryEater.Allocate();
+        GC.Collect();
+        Console.ReadKey();
+    }
+}
