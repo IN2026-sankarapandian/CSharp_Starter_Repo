16ac3fd7927a176f60491be669b63baedc8b5785
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-09-06 18:14:30 +0530

    docs: Addedcomments for enums

--- src/Reflections/Enums/Typemember.cs
+++ src/Reflections/Enums/Typemember.cs
@@ -1,9 +1,27 @@
 ﻿namespace Reflections.Enums;
 
+/// <summary>
+/// Denotes the kind of member of a type.
+/// </summary>
 public enum TypeMember
 {
+    /// <summary>
+    /// Specifies the property of an type.
+    /// </summary>
     Property,
+
+    /// <summary>
+    /// Specifies the field of an type.
+    /// </summary>
     Field,
+
+    /// <summary>
+    /// Specifies the method of an type.
+    /// </summary>
     Method,
-    Event
+
+    /// <summary>
+    /// Specifies the event of an type.
+    /// </summary>
+    Event,
 }
