f9810098dc4125ebd332b5935ad1db2afefd1a60
Sankarapandian Ganesan <sankarapandian.g@solitontech.com>
2025-09-16 12:03:23 +0530

    feat : Added keyboard app

---
+++ src/MusicalNotes/Constants/NotesResources.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicalNotes.Constants;
+
+/// <summary>
+/// Contains constants related to notes.
+/// </summary>
+public static class NotesResources
+{
+    /// <summary>
+    /// Duration of the note to play
+    /// </summary>
+    public const int Duration = 1000;
+}
