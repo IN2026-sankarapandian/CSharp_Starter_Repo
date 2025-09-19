5392cbf00bffe45be2dbf17cd29f0ae595f7d576
IN2026-sankarapandian <sankarapandian.g@solitontech.com>
2025-09-19 18:41:32 +0530

    feat : merge assignment-16 (advanced-language-features)

---
+++ src/CSharpAdvanced/Constants/Message.cs
@@ -0,0 +1,202 @@
+﻿namespace CSharpAdvanced.Constants;
+
+/// <summary>
+/// Have constants related to messages shown to the user.
+/// </summary>
+public static class Messages
+{
+    /// <summary>
+    /// Description message for anonymous methods task.
+    /// </summary>
+    public const string AnonymousDescription = "This task demonstrate the the working of anonymous methods\n";
+
+    /// <summary>
+    /// Message displayed after sorting arrays.
+    /// </summary>
+    public const string ArrayAfterSorting = "\nArray after sorting : ";
+
+    /// <summary>
+    /// Message displayed before sorting arrays.
+    /// </summary>
+    public const string ArrayBeforeSorting = "Array before sorting : ";
+
+    /// <summary>
+    /// Message displaying book details with placeholders.
+    /// </summary>
+    public const string BookDetails = "Title : {0} Author : {1} ISBN : {2}";
+
+    /// <summary>
+    /// Message displayed before displaying books data.
+    /// </summary>
+    public const string Books = "\nBooks : ";
+
+    /// <summary>
+    /// Description message for delegates advanced
+    /// </summary>
+    public const string DelegatesAdvancedDescription = "This task demonstrate the the working of delegates(Advanced)\n\n";
+
+    /// <summary>
+    /// Message displays details of circle with placeholders.
+    /// </summary>
+    public const string DisplayCircle = "It is a circle in {0} color with radius : {1}, area : {2:f2}.";
+
+    /// <summary>
+    /// Message displays before deconstruction of book details.
+    /// </summary>
+    public const string DisplayDetailsUsingDeconstruction = "\nDisplaying the details of book using deconstruction";
+
+    /// <summary>
+    /// Message displays product details with placeholders.
+    /// </summary>
+    public const string DisplayProduct = "Name : {0}, Category : {1} price {2}";
+
+    /// <summary>
+    /// Message displays rectangle details with placeholders
+    /// </summary>
+    public const string DisplayRectangle = "It is a rectangle in {0} color with height : {1}, width : {2}, area : {3}.";
+
+    /// <summary>
+    /// Message displays the information about displaying shapes.
+    /// </summary>
+    public const string DisplayShapes = "The following details are about shape objects with different properties, all these shapes types are found with pattern matching.\n";
+
+    /// <summary>
+    /// Message displays triangle details with placeholders
+    /// </summary>
+    public const string DisplayTriangle = "It is a triangle in {0} color with base : {1}, height : {2}, area : {3}.";
+
+    /// <summary>
+    /// Message displays value and type of dynamic variable with placeholders.
+    /// </summary>
+    public const string DynamicAndType = "dynamic {0} = {1}, Type : {2}";
+
+    /// <summary>
+    /// Prompt message displayed to user to enter message.
+    /// </summary>
+    public const string EnterMessagePrompt = "Enter the message : ";
+
+    /// <summary>
+    /// Prompt message displayed to the user to enter desired task to run.
+    /// </summary>
+    public const string EnterTask = "Enter which task to run : ";
+
+    /// <summary>
+    /// Warning message displayed to the user to enter a valid option
+    /// </summary>
+    public const string EnterValidOption = "Enter a valid option !";
+
+    /// <summary>
+    /// Description message of event and delegates.
+    /// </summary>
+    public const string EventAndDelegateDescription = "This task demonstrates the working of events and delegates \nWrite any message as a input to trigger the notifier class that displays the info about your message.\n\n";
+
+    /// <summary>
+    /// Prompt message displayed to user about exit option with placeholder.
+    /// </summary>
+    public const string Exit = "{0}. Exit";
+
+    /// <summary>
+    /// Warning message displayed to the user the input cannot be empty.
+    /// </summary>
+    public const string InputCannotBeEmpty = "Input cannot be empty";
+
+    /// <summary>
+    /// Message displaying that two values is equal with placeholders.
+    /// </summary>
+    public const string IsEqual = "\nIs {0} == {1} : {2}";
+
+    /// <summary>
+    /// Description message of lambda expression.
+    /// </summary>
+    public const string LambdaExpressionDescription = "This task demonstrate the the working of lambda expressions\n\n";
+
+    /// <summary>
+    /// Message displaying a message with placeholder.
+    /// </summary>
+    public const string Message = "Message : {0}";
+
+    /// <summary>
+    /// Message displaying the length of the message with placeholder.
+    /// </summary>
+    public const string MessageLength = "Length of the message : {0}";
+
+    /// <summary>
+    /// Message displays the message in uppercase with placeholder.
+    /// </summary>
+    public const string MessageUppercase = "Message in upper case : {0}";
+
+    /// <summary>
+    /// Message displayed to user to before display new book value.
+    /// </summary>
+    public const string NewBook = "New book : ";
+
+    /// <summary>
+    /// Message displayed to user to display original book value.
+    /// </summary>
+    public const string OriginalBook = "\nOriginal book : ";
+
+    /// <summary>
+    /// Description message of pattern matching.
+    /// </summary>
+    public const string PatternMatchingDescription = "This task demonstrate the the working of pattern matching\n\n";
+
+    /// <summary>
+    /// Description message of records.
+    /// </summary>
+    public const string RecordsDescription = "This task demonstrate the working of records\n\n";
+
+    /// <summary>
+    /// Message displayed to user before displaying category sorted data.
+    /// </summary>
+    public const string SortedByCategory = "\nSorted by category : ";
+
+    /// <summary>
+    /// Message displayed to user before displaying name sorted data.
+    /// </summary>
+    public const string SortedByName = "Sorted by name : ";
+
+    /// <summary>
+    /// Message displayed to user before displaying price sorted data.
+    /// </summary>
+    public const string SortedByPrice = "\nSorted by price : ";
+
+    /// <summary>
+    /// message displayed to the user before displaying squared odd numbers
+    /// </summary>
+    public const string SquaredOddNumbers = "\nSquared odd numbers : ";
+
+    /// <summary>
+    /// Prompt message displayed to the user to inform press enter to exit the task.
+    /// </summary>
+    public const string TaskExitPrompt = "\n\nPress enter to exit task {0}";
+
+    /// <summary>
+    /// Message displayed to the user to inform notifications were triggered.
+    /// </summary>
+    public const string TriggeringNotifications = "Triggering notifications..";
+
+    /// <summary>
+    /// Message displays the keyword unknown shape.
+    /// </summary>
+    public const string UnknownShape = "Unknown shape";
+
+    /// <summary>
+    /// Description message of var and dynamic.
+    /// </summary>
+    public const string VarAndDynamicDescription = "This task demonstrate the working of var and dynamic keyword\n\n";
+
+    /// <summary>
+    /// Warning message about var type assignment causing runtime error.
+    /// </summary>
+    public const string VarRuntimeTypeChangeWarning = "The type of {0} can't be changed which will cause a runtime error\n";
+
+    /// <summary>
+    /// Message about dynamic variable value change.
+    /// </summary>
+    public const string DynamicRuntimeTypeChange = "After changing the value of dynamic variable with another type";
+
+    /// <summary>
+    /// Message displaying var and type value with placeholders.
+    /// </summary>
+    public const string VarAndType = "var {0} = {1}, Type : {2}";
+}
---
+++ src/CSharpAdvanced/Constants/Titles.cs
@@ -0,0 +1,47 @@
+﻿namespace CSharpAdvanced.Constants;
+
+/// <summary>
+/// Have constants titles of the task.
+/// </summary>
+public static class Titles
+{
+    /// <summary>
+    /// Title message of the console app.
+    /// </summary>
+    public const string AppTitle = "Advanced languages features";
+
+    /// <summary>
+    /// Title message of task 1 displayed to the user.
+    /// </summary>
+    public const string TaskTitle1 = "Event and Delegates";
+
+    /// <summary>
+    /// Title message of task 2 displayed to the user.
+    /// </summary>
+    public const string TaskTitle2 = "Var and dynamic";
+
+    /// <summary>
+    /// Title message of task 3 displayed to the user.
+    /// </summary>
+    public const string TaskTitle3 = "Anonymous methods";
+
+    /// <summary>
+    /// Title message of task 4 displayed to the user.
+    /// </summary>
+    public const string TaskTitle4 = "Lambda expressions";
+
+    /// <summary>
+    /// Title message of task 5 displayed to the user.
+    /// </summary>
+    public const string TaskTitle5 = "Delegates (Advanced)";
+
+    /// <summary>
+    /// Title message of task 6 displayed to the user.
+    /// </summary>
+    public const string TaskTitle6 = "Records";
+
+    /// <summary>
+    /// Title message of task 7 displayed to the user.
+    /// </summary>
+    public const string TaskTitle7 = "Pattern matching";
+}
