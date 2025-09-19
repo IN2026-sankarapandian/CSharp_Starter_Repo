dbf549ce1b4b1429a6eca18521f2231726e607ce
IN2026-sankarapandian <sankarapandian.g@solitontech.com>
2025-09-19 18:16:39 +0530

    feat : merge assignment-15 (files-and-streams)

---
+++ src/FilesAndStreams/Enums/FileReader.cs
@@ -0,0 +1,17 @@
+﻿namespace FilesAndStreams.Enums;
+
+/// <summary>
+/// Specifies the file reader type.
+/// </summary>
+public enum FileReader
+{
+    /// <summary>
+    /// Specifies the file reader type is file stream.
+    /// </summary>
+    FileStream,
+
+    /// <summary>
+    /// Specifies the file reader type is buffered stream.
+    /// </summary>
+    BufferedStream,
+}
