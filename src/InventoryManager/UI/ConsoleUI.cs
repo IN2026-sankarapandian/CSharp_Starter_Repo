bd628aa6e7d273383bc8ce0f1ffc7b835bf5a406
IN2026-sankarapandian <sankarapandian.g@solitontech.com>
2025-09-22 09:28:08 +0530

    feat : merge assignment-6 (unit-test)

--- src/InventoryManager/UI/ConsoleUI.cs
+++ src/InventoryManager/UI/ConsoleUI.cs
@@ -7,15 +7,10 @@ namespace InventoryManager.UI;
 /// <summary>
 /// Has functions related to console UI manipulations.
 /// </summary>
-public class ConsoleUI
+public class ConsoleUI : IUserInterface
 {
-    /// <summary>
-    /// Prompt the user and returns the user input for the prompt.
-    /// </summary>
-    /// <param name="prompt">Prompt shown to the user.</param>
-    /// <param name="color">Color for the prompt.</param>
-    /// <returns>User input</returns>
-    public static string PromptAndGetInput(string prompt, ConsoleColor color = ConsoleColor.White)
+    /// <inheritdoc/>
+    public string PromptAndGetInput(string prompt, ConsoleColor color = ConsoleColor.White)
     {
         do
         {
@@ -25,7 +20,7 @@ public class ConsoleUI
             string? userInput = Console.ReadLine();
             if (string.IsNullOrEmpty(userInput))
             {
-                PromptLine(ErrorMessages.EmptyInput, ConsoleColor.Yellow);
+                this.PromptLine(ErrorMessages.EmptyInput, ConsoleColor.Yellow);
                 continue;
             }
             else
@@ -36,24 +31,16 @@ public class ConsoleUI
         while (true);
     }
 
-    /// <summary>
-    /// Prompt the user with info with specified color.
-    /// </summary>
-    /// <param name="info">Info to shown.</param>
-    /// <param name="color">Color for the prompt.</param>
-    public static void Prompt(string info, ConsoleColor color = ConsoleColor.White)
+    /// <inheritdoc/>
+    public void Prompt(string info, ConsoleColor color = ConsoleColor.White)
     {
         Console.ForegroundColor = color;
         Console.Write(info);
         Console.ResetColor();
     }
 
-    /// <summary>
-    /// Prompt the user with info in new line with specified color.
-    /// </summary>
-    /// <param name="prompt">Prompt to show.</param>
-    /// <param name="color">Color for the prompt. Default value is white.</param>
-    public static void PromptLine(string? prompt, ConsoleColor color = ConsoleColor.White)
+    /// <inheritdoc/>
+    public void PromptLine(string? prompt, ConsoleColor color = ConsoleColor.White)
     {
         if (string.IsNullOrEmpty(prompt))
         {
@@ -65,15 +52,12 @@ public class ConsoleUI
         Console.ResetColor();
     }
 
-    /// <summary>
-    /// Prints list of <see cref="Product"/>s as a table in the console.
-    /// </summary>
-    /// <param name="currentProductList">Product list to print.</param>
-    public static void PrintAsTable(List<Product> currentProductList)
+    /// <inheritdoc/>
+    public void PrintAsTable(List<Product> currentProductList)
     {
         Dictionary<string, Type> productTemplate = Product.GetTemplate();
         string[] fields = new string[] { "Index" }.Concat(productTemplate.Keys.ToArray()).ToArray();
-        ConsoleTable table = new ConsoleTable(fields);
+        ConsoleTable table = new (fields);
         for (int index = 0; index < currentProductList.Count; index++)
         {
             object[] values = new object[fields.Length];
@@ -89,24 +73,19 @@ public class ConsoleUI
         table.Write();
     }
 
-    /// <summary>
-    /// Wait for the keypress and clear the console and navigate back to the menu.
-    /// </summary>
-    public static void WaitAndNavigateToMenu()
+    /// <inheritdoc/>
+    public void WaitAndNavigateToMenu()
     {
         Console.WriteLine("\n\n\nPress any key to leave to menu...");
         Console.ReadKey();
-        ConsoleUI.CreateNewPageFor("Menu");
+        this.CreateNewPageFor("Menu");
     }
 
-    /// <summary>
-    /// Clears the console and create new page for the actions.
-    /// </summary>
-    /// <param name="action">Current action name.</param>
-    public static void CreateNewPageFor(string action)
+    /// <inheritdoc/>
+    public void CreateNewPageFor(string action)
     {
         Console.Clear();
-        Prompt($"Inventory manager - {action}", ConsoleColor.DarkBlue);
+        this.Prompt($"Inventory manager - {action}", ConsoleColor.DarkBlue);
         Console.WriteLine("\n");
     }
 }
